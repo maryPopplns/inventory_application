f4dae75920011767626c6c0d1bcd0b13f54348e3
Spencer Knight <spencer.knightusn@gmail.com>
2022-01-18 11:27:23 -0700

    added validation for types array

---
+++ models/pokemon.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PokemonSchema = new Schema({
+  name: { type: String, required: true, maxLength: 20 },
+  types: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Type',
+      validate: [(input) => input.length < 10, 'too many types'],
+    },
+  ],
+});
+
+// Virtual for author's URL
+PokemonSchema.virtual('url').get(function () {
+  return '/pokemon/' + this._id;
+});
+
+module.exports = mongoose.model('Pokemon', PokemonSchema);
