4abbfd62628e813bd9c45ae92087c538ee9e5de5
Spencer Knight <spencer.knightusn@gmail.com>
2022-01-18 08:18:30 -0700

    removed winston boilerplate

--- app.js
+++ app.js
@@ -5,36 +5,14 @@ const cookieParser = require('cookie-parser');
 const winston = require('winston');
 const morgan = require('morgan');
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-    new winston.transports.Console({ level: 'info' }),
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'logs/combined.log' }),
-  ],
-});
-
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
-// winston.configure({
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use((req, res, next) => {
-  // console.log(req.body);
-  // logger.info(req.body);
-  logger.info(res.ip);
-  next();
-});
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
