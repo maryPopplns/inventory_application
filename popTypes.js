be31c39f133f9a456220a33ab571937a259a1253
Spencer Knight <spencer.knightusn@gmail.com>
2022-01-19 11:16:23 -0700

    successfully pulling data from list of endpoints

--- popTypes.js
+++ popTypes.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const async = require('async');
 const winston = require('winston');
 const mongoose = require('mongoose');
 const axios = require('axios').default;
@@ -6,6 +7,7 @@ const Type = require(path.join(__dirname, '/models/type'));
 const Pokemon = require(path.join(__dirname, '/models/pokemon'));
 require('dotenv').config();
 
+// connect to mongo
 mongoose.connect(process.env.MONGO_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -19,20 +21,50 @@ const logger = winston.createLogger({
   defaultMeta: { service: 'user-service' },
   transports: [
     new winston.transports.Console(),
-    // new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
     // new winston.transports.File({ filename: 'logs/all.log' }),
   ],
 });
 
-for (let i = 1; i < 3; i++) {
-  (async function getUser() {
-    try {
-      const response = await axios.get(`https://pokeapi.co/api/v2/type/${i}`);
-      console.log(response.data.name);
-    } catch (error) {
-      logger.error(error);
-    }
-  })();
+// create endpoints
+const endpoints = [];
+for (let i = 1; i < 19; i++) {
+  endpoints.push(`https://pokeapi.co/api/v2/type/${i}`);
 }
 
-mongoose.connection.close();
+async.waterfall(
+  [
+    function (callback) {
+      let one = [];
+      axios
+        .all(endpoints.map((endpoint) => axios.get(endpoint)))
+        .then((data) => {
+          data.forEach((endpoint) => {
+            one.push(endpoint.data.name);
+          });
+          callback(null, one);
+        })
+        .catch(function (error) {
+          callback(error, one);
+        });
+    },
+  ],
+  function (err, result) {
+    if (err) {
+      logger.error(err);
+    }
+    console.log(result);
+    mongoose.connection.close();
+  }
+);
+
+// const type = new Type({
+//   name: name,
+// });
+// // save or log errors
+// type.save(function (err) {
+//   if (err) {
+//     logger.error(err);
+//     return;
+//   }
+// });
