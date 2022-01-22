a035adbe63e3dd4701669a4f8689ebea94084b25
Spencer Knight <spencer.knightusn@gmail.com>
2022-01-21 20:27:53 -0700

    move schema complete

--- models/move.js
+++ models/move.js
@@ -2,19 +2,17 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const TypesSchema = new Schema({
+const MoveSchema = new Schema({
   name: { type: String, required: true, maxLength: 20 },
-  doubleDamageFrom: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
-  doubleDamageTo: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
-  halfDamageFrom: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
-  halfDamageTo: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
-  noDamageFrom: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
-  noDamageTo: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
+  effect: String,
+  power: Number,
+  PP: Number,
+  type: { type: Schema.Types.ObjectId, ref: 'Type', required: true },
 });
 
 // Virtual for author's URL
-TypesSchema.virtual('url').get(function () {
-  return '/type/' + this._id;
+MoveSchema.virtual('url').get(function () {
+  return '/move/' + this._id;
 });
 
-module.exports = mongoose.model('Type', TypesSchema);
+module.exports = mongoose.model('Move', TypesSchema);
--- models/type.js
+++ models/type.js
@@ -4,12 +4,12 @@ const Schema = mongoose.Schema;
 
 const TypesSchema = new Schema({
   name: { type: String, required: true, maxLength: 20 },
-  doubleDamageFrom: [{ type: Schema.Types.ObjectId, ref: 'Types' }],
-  doubleDamageTo: [{ type: Schema.Types.ObjectId, ref: 'Types' }],
-  halfDamageFrom: [{ type: Schema.Types.ObjectId, ref: 'Types' }],
-  halfDamageTo: [{ type: Schema.Types.ObjectId, ref: 'Types' }],
-  noDamageFrom: [{ type: Schema.Types.ObjectId, ref: 'Types' }],
-  noDamageTo: [{ type: Schema.Types.ObjectId, ref: 'Types' }],
+  doubleDamageFrom: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
+  doubleDamageTo: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
+  halfDamageFrom: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
+  halfDamageTo: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
+  noDamageFrom: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
+  noDamageTo: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
 });
 
 // Virtual for author's URL
@@ -17,4 +17,4 @@ TypesSchema.virtual('url').get(function () {
   return '/type/' + this._id;
 });
 
-module.exports = mongoose.model('Types', TypesSchema);
+module.exports = mongoose.model('Type', TypesSchema);
