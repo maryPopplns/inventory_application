a9ffa9b00af08007b2bef7d7195c9106b16f914e
Spencer Knight <spencer.knightusn@gmail.com>
2022-01-21 19:25:17 -0700

    changed type shema ref name

---
+++ models/move.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const TypesSchema = new Schema({
+  name: { type: String, required: true, maxLength: 20 },
+  doubleDamageFrom: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
+  doubleDamageTo: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
+  halfDamageFrom: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
+  halfDamageTo: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
+  noDamageFrom: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
+  noDamageTo: [{ type: Schema.Types.ObjectId, ref: 'Type' }],
+});
+
+// Virtual for author's URL
+TypesSchema.virtual('url').get(function () {
+  return '/type/' + this._id;
+});
+
+module.exports = mongoose.model('Type', TypesSchema);
