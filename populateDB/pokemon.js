f1107c7523f2b2c3e5a0dc819ae701eaded286e6
Spencer Knight <spencer.knightusn@gmail.com>
2022-01-22 16:38:46 -0700

    removed ..logs from git

--- populateDB/pokemon.js
+++ populateDB/pokemon.js
@@ -18,12 +18,12 @@ const { endianness } = require('os');
 require('dotenv').config();
 
 // [ MONGO CONNECTION ]
-mongoose.connect(process.env.MONGO_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// mongoose.connect(process.env.MONGO_STRING, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // [ WINSTON LOGGER ]
 const logger = winston.createLogger({
@@ -33,13 +33,15 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.Console(),
     new winston.transports.File({
-      filename: 'logs/error.log',
+      filename: 'populateDB/logs/error.log',
       level: 'error',
     }),
     new winston.transports.File({
-      filename: 'logs/info.log',
+      filename: 'populateDB/logs/info.log',
       level: 'info',
     }),
     // new winston.transports.File({ filename: 'logs/all.log' }),
   ],
 });
+
+logger.info(chalk.red('hola mundo'));
