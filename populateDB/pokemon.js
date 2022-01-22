48575a11fcf70e7f141401c894c1e7159999063b
Spencer Knight <spencer.knightusn@gmail.com>
2022-01-22 16:15:21 -0700

    pokemon db populate boilerplate complete

---
+++ populateDB/pokemon.js
@@ -0,0 +1,45 @@
+// [ MODELS ]
+const path = require('path');
+const Type = require(path.join(__dirname, '../models/type'));
+const Pokemon = require(path.join(__dirname, '../models/pokemon'));
+const Move = require(path.join(__dirname, '../models/move'));
+// [ COMMAND LINE ]
+const chalk = require('chalk');
+const winston = require('winston');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
+
+const async = require('async');
+const mongoose = require('mongoose');
+const axios = require('axios').default;
+const req = require('express/lib/request');
+const { endianness } = require('os');
+require('dotenv').config();
+
+// [ MONGO CONNECTION ]
+mongoose.connect(process.env.MONGO_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// [ WINSTON LOGGER ]
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: 'logs/error.log',
+      level: 'error',
+    }),
+    new winston.transports.File({
+      filename: 'logs/info.log',
+      level: 'info',
+    }),
+    // new winston.transports.File({ filename: 'logs/all.log' }),
+  ],
+});
