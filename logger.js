73d350bc8c06dfb980460ba06ef13525765865a8
Spencer Knight <spencer.knightusn@gmail.com>
2022-01-22 18:17:02 -0700

    added colors to logger

---
+++ logger.js
@@ -0,0 +1,31 @@
+const winston = require('winston');
+
+let alignColorsAndTime = winston.format.combine(
+  winston.format.colorize({
+    all: true,
+  }),
+  winston.format.label({
+    label: '[LOGGER]',
+  }),
+  winston.format.timestamp({
+    format: 'YY-MM-DD HH:MM:SS',
+  }),
+  winston.format.printf(
+    (info) =>
+      ` ${info.label}  ${info.timestamp}  ${info.level} : ${info.message}`
+  )
+);
+
+const logger = winston.createLogger({
+  level: 'debug',
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        alignColorsAndTime
+      ),
+    }),
+  ],
+});
+
+module.exports = logger;
--- populateDB/pokemon.js
+++ populateDB/pokemon.js
@@ -5,7 +5,8 @@ const Pokemon = require(path.join(__dirname, '../models/pokemon'));
 const Move = require(path.join(__dirname, '../models/move'));
 // [ COMMAND LINE ]
 const chalk = require('chalk');
-const winston = require('winston');
+const logger = require(path.join(__dirname, '../logger'));
+// const winston = require('winston');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 const argv = yargs(hideBin(process.argv)).argv;
@@ -26,22 +27,22 @@ require('dotenv').config();
 // db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // [ WINSTON LOGGER ]
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
-      filename: 'populateDB/logs/error.log',
-      level: 'error',
-    }),
-    new winston.transports.File({
-      filename: 'populateDB/logs/info.log',
-      level: 'info',
-    }),
-    // new winston.transports.File({ filename: 'logs/all.log' }),
-  ],
-});
+// const logger = winston.createLogger({
+//   level: 'info',
+//   format: winston.format.json(),
+//   defaultMeta: { service: 'user-service' },
+//   transports: [
+//     new winston.transports.Console(),
+//     new winston.transports.File({
+//       filename: 'populateDB/logs/error.log',
+//       level: 'error',
+//     }),
+//     new winston.transports.File({
+//       filename: 'populateDB/logs/info.log',
+//       level: 'info',
+//     }),
+//     // new winston.transports.File({ filename: 'logs/all.log' }),
+//   ],
+// });
 
-logger.info(chalk.red('hola mundo'));
+logger.error('hola mundo');
