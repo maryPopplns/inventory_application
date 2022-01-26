0a9ab7582e0da35a84c4927f81b8e88141778e23
Spencer Knight <spencer.knightusn@gmail.com>
2022-01-26 15:16:22 -0700

    changed animation wrap

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,18 +5,23 @@ const Move = require(path.join(__dirname, '../models/move'));
 const Pokemon = require(path.join(__dirname, '../models/pokemon'));
 
 exports.home_get = function (req, res, next) {
+  // [ QUERY/FILTER POKEMON DATA ]
   Pokemon.find({})
     .limit(3)
     .select('name images.animation')
     .then((pokemon) => {
       const filteredPokemonData = pokemon.map((pokemon) => {
+        const name = Array.from(pokemon.name)
+          .map((letter, index) => (index === 0 ? letter.toUpperCase() : letter))
+          .join('');
         return {
-          name: pokemon.name,
+          name: name,
           animation: pokemon.images.animation,
           id: pokemon.id,
         };
       });
       logger.debug(filteredPokemonData);
+      // [ RENDER GIF ANIMATIONS ]
       res.render('home', { data: filteredPokemonData });
     })
     .catch((err) => logger.error(err));
