7029a7ae8197c8a1564abeb4970e6288f884937d
Spencer Knight <spencer.knightusn@gmail.com>
2022-01-30 10:37:19 -0700

    added source mapping and hmr to babel command

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,9 @@
-'use strict';
+"use strict";
+
+(function movesTypesSlider() {
+  var movesSlider = document.getElementsByClassName('movesSlider');
+  var typesSliders = document.getElementsByClassName('typesSlider');
+  console.log(movesSlider);
+  console.log(typesSliders);
+})();
+//# sourceMappingURL=index.js.map
