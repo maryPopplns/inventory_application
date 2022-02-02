54b699a0d6ecf008dc3d7e56639e16675a6eaedb
Spencer Knight <spencer.knightusn@gmail.com>
2022-02-01 17:49:41 -0700

    slider complete

--- public/javascripts/updatePokemonGet.js
+++ public/javascripts/updatePokemonGet.js
@@ -2,5 +2,12 @@
 
 (function () {
   var updatePokeTypesSlider = document.getElementsByClassName('updatePokemonTypesSlider')[0];
+
+  var updatePokeTypesHandler = function updatePokeTypesHandler() {
+    var typesContent = document.getElementsByClassName('updatePokemonTypesContent')[0];
+    typesContent.classList.toggle('open'); // TODO change the arrow button
+  };
+
+  updatePokeTypesSlider.addEventListener('click', updatePokeTypesHandler);
 })();
 //# sourceMappingURL=updatePokemonGet.js.map
--- src/javascripts/updatePokemonGet.js
+++ src/javascripts/updatePokemonGet.js
@@ -2,4 +2,14 @@
   const updatePokeTypesSlider = document.getElementsByClassName(
     'updatePokemonTypesSlider'
   )[0];
+
+  const updatePokeTypesHandler = () => {
+    const typesContent = document.getElementsByClassName(
+      'updatePokemonTypesContent'
+    )[0];
+    typesContent.classList.toggle('open');
+    // TODO change the arrow button
+  };
+
+  updatePokeTypesSlider.addEventListener('click', updatePokeTypesHandler);
 })();
